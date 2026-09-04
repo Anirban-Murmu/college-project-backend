0b23759b0f52451f577ba829824cd2c34920bc32
murmuanirban39-coder <murmuanirban39@gmail.com>
2026-09-04 10:01:37 +0530

    Improve production error logging

--- config/settings/base.py
+++ config/settings/base.py
@@ -360,3 +360,23 @@ FIREBASE_DATABASE_URL = config("FIREBASE_DATABASE_URL")
 
 
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
+}
