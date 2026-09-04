f8b5944ae6e36e357152a61b6c58c2b586be8b26
murmuanirban39-coder <murmuanirban39@gmail.com>
2026-09-04 05:42:37 +0530

    Fix Django email configuration

--- config/settings/base.py
+++ config/settings/base.py
@@ -202,19 +202,35 @@ AUTH_USER_MODEL = "accounts.User"
 # EMAIL
 # ============================================================
 
-MAILERS = {
-    "default": {
-        "BACKEND": "django.core.mail.backends.smtp.EmailBackend",
-        "OPTIONS": {
-            "host": config("EMAIL_HOST"),
-            "port": config("EMAIL_PORT", cast=int),
-            "username": config("EMAIL_HOST_USER"),
-            "password": config("EMAIL_HOST_PASSWORD"),
-            "use_tls": config("EMAIL_USE_TLS", cast=bool),
-            "timeout": 10,
-        },
-    },
-}
+# MAILERS = {
+#     "default": {
+#         "BACKEND": "django.core.mail.backends.smtp.EmailBackend",
+#         "OPTIONS": {
+#             "host": config("EMAIL_HOST"),
+#             "port": config("EMAIL_PORT", cast=int),
+#             "username": config("EMAIL_HOST_USER"),
+#             "password": config("EMAIL_HOST_PASSWORD"),
+#             "use_tls": config("EMAIL_USE_TLS", cast=bool),
+#             "timeout": 10,
+#         },
+#     },
+# }
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_PORT = config("EMAIL_PORT", cast=int)
+
+EMAIL_USE_TLS = config(
+    "EMAIL_USE_TLS",
+    default=True,
+    cast=bool,
+)
+
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+
+EMAIL_TIMEOUT = 10
 
 DEFAULT_FROM_EMAIL = config("EMAIL_HOST_USER")
 
